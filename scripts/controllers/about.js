6c22cc53663d2b758ef008e90306fa0bc188ff2e
zackforhack <zackforhack@gmail.com>
2015-03-05 00:02:25 -0500

    fix
    fix

--- scripts/controllers/about.js
+++ scripts/controllers/about.js
@@ -13,10 +13,10 @@ angular.module('appApp')
         
 
             var newAbout = {
-                'Address': profile.Address,
-                'Name': profile.Name,
-                'Telephone': profile.Telephone,
-                'Hours': profile.Hours
+                'Address': $scope.profile.Address,
+                'Name': $scope.profile.Name,
+                'Telephone': $scope.profile.Telephone,
+                'Hours': $scope.profile.Hours
             };
 
         $scope.about.$add(angular.fromJson(angular.toJson(newAbout)));
